327900d6b56682b9d8afcfc1f573517626cc8188
ron <rinbar70@gmail.com>
2020-07-04 22:48:10 +0300

    Updates

---
+++ precache-manifest.eb0a196efe1a28bd7d082dd56ce2ea5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47761a2c70c09eced35cfdb652b1a36b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afbe7486f167bcc3bc19",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "418e8d03e9926ea2d602",
+    "url": "/static/js/2.70c39de7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.70c39de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afbe7486f167bcc3bc19",
+    "url": "/static/js/main.a188745b.chunk.js"
+  },
+  {
+    "revision": "7f17e3984274356b91a4",
+    "url": "/static/js/runtime-main.637d699e.js"
+  }
+]);
--- src/components/Comparison.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Comparison() {
-    return <h1>Comparison</h1>;
-}
