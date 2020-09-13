9bf89c0857e5156a061d800d4bf2883f26db050a
ron <rinbar70@gmail.com>
2020-09-14 00:02:37 +0300

    Show deaths per million people.

--- src/components/CountriesSelect.js
+++ src/components/CountriesSelect.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ({ countries, onChange }) {
     return (
-        <select multiple onChange={onChange}>
+        <select multiple onChange={onChange} size={20}>
             {countries.map(({ Country: country, Slug: slug, ISO2: iso2 }) => (
                 <option key={iso2} value={slug}>{country}</option>
             ))}
