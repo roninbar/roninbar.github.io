eee3ef596d8aa3c9f1697e4a63aa308fbff88103
ron <rinbar70@gmail.com>
2020-09-14 17:12:47 +0300

    Add a 'size' prop to <CountriesSelect>.

--- src/components/CountriesSelect.js
+++ src/components/CountriesSelect.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function ({ countries, onChange }) {
+export default function ({ countries, onChange, size = 20 }) {
     return (
-        <select multiple onChange={onChange} size={20}>
+        <select multiple onChange={onChange} size={size}>
             {countries.map(({ Country: country, Slug: slug, ISO2: iso2 }) => (
                 <option key={iso2} value={slug}>{country}</option>
             ))}
